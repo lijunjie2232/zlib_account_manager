22f2306bf64ab736baebb500fdc6a95e9caa2eef
lijunjie2232 <2533584225@qq.com>
2025-10-18 13:40:56 +0900

    update info

--- src/tamp_index.js
+++ src/tamp_index.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Floating button to manage multiple Z-Library accounts, supporting switching, saving, and editing account information
-// @author       Assistant
+// @author       lijunjie2232
 // @match        *://*z-library.*/*
 // @grant        GM_setValue
 // @grant        GM_getValue
